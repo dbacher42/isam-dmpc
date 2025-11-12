7a2c7fa6ddc1671cfe3564b85d3f17601e688358
avisoval <avi.soval@outlook.com>
2025-11-12 01:50:28 -0800

    sim exec builds agents which internally build controller, common dt enforced

--- Agent.py
+++ Agent.py
@@ -1,40 +1,64 @@
 import numpy as np
+from lqr import FloatbotLQR
+from model import FloatbotModel
+from mpc import FloatbotMPC
 
 # -------------------------------------------------------------------------------------------------
 
 class Agent():
     """
     
-        Simulates a single floatbot agent with a given
-        system model and controller. 
+        Builds and simulates a single floatbot agent. 
 
         Can read/write to the Oracle during sim. 
     
     """
 
-    def __init__(self, model, controller, x0=np.array([0, 0, 1, 0, 0, 0, 0])):
+    def __init__(self, model, dt, x0=np.array([0, 0, 1, 0, 0, 0, 0]), name=None):
         """
             Parameters
             ----------
                 model : floatbot kinematics and dynamics model
-                controller : floatbot controller
                 x0 : initial states
                     optional, the default is np.array([0, 0, 1, 0, 0, 0, 0]).
         """
 
-        # Set simulation parameters
-        self.model = model
-        self.controller = controller
-        self.dt = controller.dt
-        
-        #Initialize data lists
+        # Agent parameters
+        self.name       = name
+        self.model      = model
+        self.dt         = dt
+
+        # Initialize data lists
         self.t_history = [0]
         self.x_history = [x0]
         self.u_history = []
         
+        # Current 
         self.x_current = x0
         self.u_current = np.zeros(8)
     
+    # --- 
+
+    def _build_controller(self, controller_type, controller_params):
+        """
+            Build controller internally with provided dt
+        """
+
+        if 'x_cmd' not in controller_params:
+            raise ValueError("Controller must be provided a reference state 'x_cmd'")
+        x_cmd = controller_params['x_cmd']
+
+        if controller_type == 'MPC':
+            H = controller_params.get('H', 10)
+            Q = controller_params.get('Q', np.eye(6))
+            R = controller_params.get('R', np.eye(4))
+            self.controller = FloatbotMPC(self.model, x_cmd, self.dt, H, Q, R)
+
+        elif controller_type == 'LQR':
+            Q = controller_params.get('Q', np.eye(6))
+            R = controller_params.get('R', np.eye(4))
+            self.controller = FloatbotLQR(self.model, x_cmd, self.dt, Q, R)
+
 
     # --- --- --- --- --- SIMULATION STEP --- --- --- --- ---
 
@@ -42,6 +66,10 @@ class Agent():
         """
             Compute control input for the current state
         """
+
+        if not hasattr(self, 'controller'):
+            raise ValueError("Controller has not been built yet. Call _build_controller() first.")
+
         # Extract parameters
         control_law = self.controller.solve
         
