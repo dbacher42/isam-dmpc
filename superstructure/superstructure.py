d9a6ad9bff2c2051d5eb4e00348a2e5042565b9e
avisoval <avi.soval@outlook.com>
2025-11-29 19:48:08 -0800

    docking sites use local block z / sizes

--- superstructure/superstructure.py
+++ superstructure/superstructure.py
@@ -105,7 +105,7 @@ class Superstructure_2D:
         
         # Compile later in sim exec
         self.spec = STRUCTURE
-        self.block_ids = [b.id for b in blocks]
+        self.blocks = {b.id: b for b in blocks}  # Store blocks for geometry lookup
     
 
 # ---
